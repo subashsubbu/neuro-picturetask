04f4555c6b9f2e64912d8aa3307103f8326358bc
subashsubbu <35239091+subashsubbu@users.noreply.github.com>
2021-06-03 13:25:34 -0500

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
-import { ExperimentWindow } from 'jspsych-react';
-import { timelineFactory } from './timeline';
-import callbackImageKeyboardResponsePlugin from './callbackImageKeyboardResponsePlugin';
-
-// Define callback function
-// const callback = (targetID) => console.log(targetID);
-// const timeline = timelineFactory(callback);
 
 ReactDOM.render(
   <App
