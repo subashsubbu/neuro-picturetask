e5f007eaf54ee511b6ce495b42c435003811fc65
subashsubbu <35239091+subashsubbu@users.noreply.github.com>
2021-06-03 13:23:35 -0500

    added app component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App'
 import { ExperimentWindow } from 'jspsych-react';
 import { timelineFactory } from './timeline';
 import callbackImageKeyboardResponsePlugin from './callbackImageKeyboardResponsePlugin';
 
 // Define callback function
-const callback = (targetID) => console.log(targetID);
-const timeline = timelineFactory(callback);
+// const callback = (targetID) => console.log(targetID);
+// const timeline = timelineFactory(callback);
 
 ReactDOM.render(
-    <ExperimentWindow
-      settings={{ timeline }}
-      plugins={{ callbackImageKeyboardResponsePlugin }}
-    />
-
-  , document.querySelector('.container'));
+  <App
+  />
+, document.querySelector('.container'));
