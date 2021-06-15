663981f7ca855fb320d4e39d2d99c7c7343d01f9
subashsubbu <35239091+subashsubbu@users.noreply.github.com>
2021-06-14 23:02:45 -0500

    added pages

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,3 +1,54 @@
+// // src/pages/Login.js
+// import React, { useState, useEffect } from "react";
+// import { LoginForm } from "../Components/LoginForm";
+
+// export function Login({ notion, user, setUser, setDeviceId }) {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [error, setError] = useState("");
+//   const [isLoggingIn, setIsLoggingIn] = useState(false);
+
+//   useEffect(() => {
+//     if (!user && notion && email && password) {
+//       login();
+//     }
+  
+//     async function login() {
+//       setIsLoggingIn(true);
+//       const auth = await notion
+//         .login({ email, password })
+//         .catch(error => {
+//           setError(error.message);
+//         });
+  
+//       if (auth) {
+//         setUser(auth.user);
+//       }
+  
+//       setIsLoggingIn(false);
+//     }
+//   }, [email, password, notion, user, setUser, setError]);
+
+//   function onLogin({ email, password, deviceId }) {
+//     if (email && password && deviceId) {
+//       setError("");
+//       setEmail(email);
+//       setPassword(password);
+//       setDeviceId(deviceId);
+//     } else {
+//       setError("Please fill the form");
+//     }
+//   }
+
+//   return (
+//     <LoginForm onLogin={onLogin} loading={isLoggingIn} error={error} />
+//   );
+// }
+
+
+
+
+
 import React, { useState, useEffect } from "react";
 import { navigate } from "@reach/router";
 import useEffectOnce from "react-use/lib/useEffectOnce";
