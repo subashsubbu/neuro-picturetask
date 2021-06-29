1b39c37cb79c486b77b5a15413cd3f4c06e61605
JVlasak99 <jvlasak99@gmail.com>
2021-06-29 09:49:14 -0500

    changed "Begin" link to a button

--- src/pages/Instructions.js
+++ src/pages/Instructions.js
@@ -5,33 +5,36 @@ import { Nav } from "../Components/Nav";
 import { useNotion } from "../services/notion";
 
 export function Instructions() {
-  const { user } = useNotion();
+    const { user } = useNotion();
 
-  if (!user) {
-    return null;
-  }
+    if (!user) {
+        return null;
+    }
 
-  return (
-    <main className="main-container">
-      {user ? <Nav /> : null}
+    return (
+        <main className="main-container">
+            {user ? <Nav /> : null}
 
-      <div>
-        <p style= {{textAlign:"center"}}>
-          Welcome to the <strong>picture viewing task</strong>! In this
-          experiment, your task will be to simply watch and pay attention
-          to the images - when no image is on the screen, please focus on
-          the "+" in the middle of the screen. <strong>Warning:</strong> some
-          of the images will contain evocative (e.g., nudity, adventure) or
-          disturbing (e.g., violence) content.
-        </p>
+            <div>
+                <p style={{ textAlign: "center" }}>
+                    Welcome to the <strong>picture viewing task</strong>! In
+                    this experiment, your task will be to simply watch and pay
+                    attention to the images - when no image is on the screen,
+                    please focus on the "+" in the middle of the screen.{" "}
+                    <strong>Warning:</strong> some of the images will contain
+                    evocative (e.g., nudity, adventure) or disturbing (e.g.,
+                    violence) content.
+                </p>
 
-        <p style= {{textAlign:"center"}}>
-          When you're ready please click "Begin" below!
-        </p>
-
-        <Link to="/experiment">Begin</Link>
-      </div>
-
-    </main>
-  );
+                <p style={{ textAlign: "center" }}>
+                    When you're ready please click "Begin" below!
+                </p>
+                <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <Link to="/experiment">
+                        <button>Begin</button>
+                    </Link>
+                </div>
+            </div>
+        </main>
+    );
 }
