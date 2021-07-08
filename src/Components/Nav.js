af77a28400aeb5501985c5e621bc8f2de9931133
JVlasak99 <jvlasak99@gmail.com>
2021-07-08 14:42:00 -0500

    going back to working state

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,3 +1,37 @@
+// // src/components/Nav.js
+// import React, { useState, useEffect } from "react";
+// import { navigate } from "@reach/router";
+
+// import { Status } from "./Status";
+// import { Footer } from "./Footer";
+
+// export function Nav({ notion }) {
+//   const [info, setInfo] = useState(null);
+
+//   useEffect(() => {
+//     if (!notion) {
+//       return;
+//     }
+
+//     notion.getInfo().then(info => {
+//       setInfo(info);
+//     });
+//   }, [notion]);
+
+//   return (
+//     <nav className="card">
+//       <Status notion={notion} info={info} />
+//       <button onClick={() => navigate("/logout")} className="card-btn">
+//         Logout
+//       </button>
+//     </nav>
+//   );
+// }
+
+
+
+
+
 import React from "react";
 import { navigate } from "@reach/router";
 
