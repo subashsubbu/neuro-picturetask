fe906b95deaca127e9063684a705e8c0229ec603
JVlasak99 <jvlasak99@gmail.com>
2021-07-08 12:53:28 -0500

    resetting to working commit

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,37 +1,3 @@
-// // src/components/Nav.js
-// import React, { useState, useEffect } from "react";
-// import { navigate } from "@reach/router";
-
-// import { Status } from "./Status";
-// import { Footer } from "./Footer";
-
-// export function Nav({ notion }) {
-//   const [info, setInfo] = useState(null);
-
-//   useEffect(() => {
-//     if (!notion) {
-//       return;
-//     }
-
-//     notion.getInfo().then(info => {
-//       setInfo(info);
-//     });
-//   }, [notion]);
-
-//   return (
-//     <nav className="card">
-//       <Status notion={notion} info={info} />
-//       <button onClick={() => navigate("/logout")} className="card-btn">
-//         Logout
-//       </button>
-//     </nav>
-//   );
-// }
-
-
-
-
-
 import React from "react";
 import { navigate } from "@reach/router";
 
